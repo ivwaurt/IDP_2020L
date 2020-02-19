7ebee7d4acf9f332ac346e130e04df946ad82804
yhw25 <yhw25@cam.ac.uk>
2020-02-19 20:07:08 +0000

    better rect detection

--- test_rect.py
+++ test_rect.py
@@ -26,6 +26,14 @@ def isofilter(img,index,bias):
     out = np.clip(out,a_min=0,a_max=255)
     return np.asarray(out, dtype='uint8' )
 
+def linrgb(img,a):
+    if(len(a))==3:
+        a.append(0)
+    img = np.asarray(img,dtype='int16')
+    out = a[0]*img[:,:,0]+a[1]*img[:,:,1]+a[2]*img[:,:,2]+a[3]
+    out = np.clip(out,a_min=0,a_max=255)
+    return np.asarray(out, dtype='uint8' )    
+    
 def valid_rect(w,h,min_area,max_area,min_ratio,max_ratio):
     w,h = int(w),int(h)
     area = w*h
@@ -49,38 +57,42 @@ while(cap.isOpened()):
     output = frame
     
     
-    #Crop frame
-    frame[:,crop[1]:width,:] = 0
-    frame[:,0:crop[0],:] = 0
-    frame[tunnel[0]:tunnel[2],tunnel[1]:tunnel[3],:] = 0
-    
-    
-    #Draw cropped area
-    cv.rectangle(output,(tunnel[1],tunnel[0]),(tunnel[3],tunnel[2]),(255,0,0),3)
-    cv.rectangle(output,(crop[0],0),(crop[1],width),(255,0,0),3)
-    
-    
     #Convert to red and blur
-    frame = isofilter(frame,2,-100)
-    frame = cv.medianBlur(frame,21)
-
+    mask_th = linrgb(frame,[1,-3,1])
+    #mask_th = isofilter(mask_th,2,-100)
+    mask_th = cv.medianBlur(mask_th,21)
+    
+    
+    #Crop frame
+    mask_th[:,crop[1]:width] = 0
+    mask_th[:,0:crop[0]] = 0
+    mask_th[tunnel[0]:tunnel[2],tunnel[1]:tunnel[3]] = 0
     
     #Tresholding
-    _,thrash = cv.threshold(frame,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
-    cv.imshow('Mask',thrash)
-    contours,_ = cv.findContours(thrash,cv.RETR_TREE,cv.CHAIN_APPROX_NONE)
+    _,mask_th = cv.threshold(mask_th,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+    cv.imshow('Mask',mask_th)
+    contours,_ = cv.findContours(mask_th,cv.RETR_TREE,cv.CHAIN_APPROX_NONE)
     for contour in contours:
         #Find bounding rectangle
         rect = cv.minAreaRect(contour)
         (cx,cy), (w,h), rot_angle = rect
         box = np.int0(cv.boxPoints(rect))
+        #print(w,h)
         
         #Draw box
         if valid_rect(w,h,min_area=750,max_area=1500,min_ratio=1,max_ratio=2):
+        #if valid_rect(w,h,min_area=10000,max_area=50000,min_ratio=3,max_ratio=8):        
             cv.drawContours(output, [box], 0, (0,255,0), 3)
     
             #Writing data
+            if(w<h):
+                rot_angle+=90
             rect_locations.append([cx,cy,rot_angle])
+    
+    #Draw cropped area
+    cv.rectangle(output,(tunnel[1],tunnel[0]),(tunnel[3],tunnel[2]),(255,0,0),3)
+    cv.rectangle(output,(crop[0],0),(crop[1],width),(255,0,0),3)
+    
     
     #Show output
     cv.imshow('output',output)
@@ -96,8 +108,8 @@ out.release()
 #Plotting recorded data
 center_x = np.array(rect_locations)[:,0]
 center_y = np.array(rect_locations)[:,1]
-center_x = np.convolve(center_x,np.ones(5),'valid')
-center_y = np.convolve(center_y,np.ones(5),'valid')
+center_x = np.convolve(center_x,np.ones(1),'valid')
+center_y = np.convolve(center_y,np.ones(1),'valid')
 plt.plot(center_x,center_y)
 plt.show()
 plt.plot(np.array(rect_locations)[:,2])
