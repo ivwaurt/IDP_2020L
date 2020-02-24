e1ecbc54fd31cc27a119f65bfe819d1b4007f08f
yhw25 <yhw25@cam.ac.uk>
2020-02-24 18:21:42 +0000

    cleaned up line following code

---
+++ Line_follow_test.ino
@@ -0,0 +1,68 @@
+/*------ Arduino Line Follower Code----- */
+/*-------definning Inputs------*/
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *motorL = AFMS.getMotor(1);
+Adafruit_DCMotor *motorR = AFMS.getMotor(2);
+
+//change motor voltage 
+/*-------definning Outputs------*/
+#define LM1 3       // left motor //(only 2 motors)
+#define RM1 4       // right motor
+
+
+//Variables
+bool sensor_l;      // 1 = white, 0 = black
+bool sensor_r;
+bool speed_l;       // 1 = faster, 0 = slower
+bool speed_r;
+
+//Parameters
+double tol_l = 3.00;  //Boundary between black/white
+double tol_r = 3.00;  
+uint8_t v = 255;      //Motor speed
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(A0, INPUT); //left sensor as input
+  pinMode(A1, INPUT); //right sensor as input
+  AFMS.begin();
+}
+
+void loop()
+{
+  //Print readings from sensors
+  Serial.println(analogRead(A0));
+  Serial.println(analogRead(A1));
+  
+  //?
+  motorL->run(FORWARD);
+  motorR->run(FORWARD);
+  
+  //Sensor reading to bool
+  sensor_l = (analogRead(A0)>tol_l) ? 1 : 0;
+  sensor_r = (analogRead(A0)>tol_r) ? 1 : 0;
+  
+  //Determine motor speeds using boolean logic
+  speed_l = sensor_l || sensor_r;
+  speed_r = ! sensor_r;
+  
+  //Set left motor speed
+  if(speed_l){
+      motorL->setSpeed(v);
+  } else {
+      motorL->setSpeed(v/2);
+  }
+  
+  //Set right motor speed
+  if(speed_r){
+      motorR->setSpeed(v);
+  } else {
+      motorR->setSpeed(v/2);
+  }
+  
+  //Delay till next loop
+  delay(500);
+}
