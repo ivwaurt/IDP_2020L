be19729f82955759bee33e20ea17a5069941478a
yhw25 <yhw25@cam.ac.uk>
2020-02-26 16:10:11 +0000

    basic actions test

---
+++ Basic_Actions.ino
@@ -0,0 +1,93 @@
+/*------ Arduino Line Follower Code----- */
+/*-------definning Inputs------*/
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *motorL = AFMS.getMotor(1);
+Adafruit_DCMotor *motorR = AFMS.getMotor(2);
+
+//change motor voltage 
+/*-------definning Outputs------*/
+#define LM1 3       // left motor //(only 2 motors)
+#define RM1 4       // right motor
+
+
+//Variables
+int speed_L;
+int speed_R;
+
+//Parameters
+double ang2t = 20.37   //time taken to rotate one degree
+double dis2t = 100     //time taken to rotate one cm
+
+uint8_t v = 128;      //Motor speed during movement
+
+
+//Functions
+
+//Update motor speed
+void motor_L(int speed){
+  if (speed == speed_L){
+    return  
+  }else{
+    if (speed>=0){
+      motorL->run(FORWARD);
+    } else {
+      motorL->run(BACKWARD);
+    }
+    motorL->setSpeed(speed);
+    speed_L = speed;
+  }
+}    
+
+void motor_R(int speed){
+  if (speed == speed_R){
+    return  
+  }else{
+    if (speed>=0){
+      motorR->run(FORWARD);
+    } else {
+      motorR->run(BACKWARD);
+    }
+    motorR->setSpeed(speed);
+    speed_R = speed;
+  }
+}    
+
+//Rotate robot x degrees clockwise
+void rotate(double angle){
+  motor_L(v);
+  motor_R(-v);
+  delay(angle*ang2t);
+  motor_L(0);
+  motor_R(0);
+}
+
+void forward(double dist){
+  motor_L((dist>=0) ? v : -v);
+  motor_R((dist>=0) ? v : -v);
+  delay(dist*dis2t);  
+  motor_L(0);
+  motor_R(0);
+}
+
+//Loop
+void setup()
+{
+  Serial.begin(9600);
+  AFMS.begin();
+  
+  
+  //Rotate 720 degrees
+  rotate(720.)
+  
+  //Wait
+  delay(2000)
+}
+
+void loop()
+{
+  //Run in a square
+  foward(50);
+  rotate(90);
+}
--- test_telnet.py
+++ test_telnet.py
@@ -3,13 +3,16 @@ import time
 
 
 
-ip = "192.168.43.156"
+ip = "192.168.4.1"
 port = 23
 conn_tout = 5
 connection = telnetlib.Telnet(ip,port,conn_tout)
 
-t = 0
-while t<1000:
-    connection.write(("Testing "+str(t)+"\n").encode('ascii'))
-    t = t+1
-    time.sleep(1)
+t=0
+while 1:
+    text = connection.read_until("HELLO".encode('ascii'),1)
+    print(text)
+    if text != '':
+        t = t+1
+        connection.write(("Testing "+str(t)+"\n").encode('ascii'))
+        time.sleep(1)
