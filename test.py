3d161e1ea57c79cbc00d2592ff70ea7b44b5a005
yhw25 <yhw25@cam.ac.uk>
2020-03-03 19:35:06 +0000

    Navigation now works with waypoints

--- test.py
+++ test.py
@@ -1,14 +1,7 @@
 
-
-v = input("control motor")
-if v=='0' :
-    byte = 0 #0000
-elif v=='1' : 
-    byte = 10 #1010
-elif v=='2' :
-   byte = 15 #1111
-byte = bytes([byte])
-
-
-print(("Testing \n").encode('ascii'))
-print(byte)
+state = -1
+msg = "OOHELLO"
+if len(msg)>5: 
+    if msg[-6].isdigit():
+        state = int(msg[-6])
+print(state)
