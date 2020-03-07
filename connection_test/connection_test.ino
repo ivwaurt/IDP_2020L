544b38316f7cb61159965b82c1961c6d726be85d
weikangtwk <wkt27@cam.ac.uk>
2020-03-07 23:16:37 +0000

    connection test added

---
+++ connection_test/connection_test.ino
@@ -0,0 +1,57 @@
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <Wire.h>
+
+//Wifi
+char ssid[] = "OnePlus 7 Pro";  //SSID
+char pass[] = "11123456";       //password
+uint8_t msg=0;
+uint8_t buf;       //Byte
+size_t siz = 1;
+int status = WL_IDLE_STATUS;
+bool alreadyConnected;
+int count=0;
+
+WiFiServer server(23);
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  msg=0;
+    //Wifi stuff
+    if (status != WiFi.status()) {
+    status = WiFi.status();
+    }
+      if (status != WL_CONNECTED){;
+      while (status != WL_CONNECTED) {
+      Serial.print("Attempting to connect to SSID: ");
+      Serial.println(ssid);
+      // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+      status = WiFi.begin(ssid, pass);
+      // wait 5 seconds for connection:
+      delay(5000);
+    }
+    server.begin();
+    alreadyConnected = false; 
+  }
+    //Read connection
+  WiFiClient client = server.available();
+  if (client) {
+    if (!alreadyConnected) {
+      // clead out the input buffer:
+      client.flush();
+      alreadyConnected = true;
+    }
+
+    if (client.available() > 0) {
+      // read the bytes incoming from the client:
+      client.read(&buf , siz);
+      msg = buf+1-1;
+    }
+  }
+  count++;
+  server.write(1);
+  Serial.println(msg);
+}
